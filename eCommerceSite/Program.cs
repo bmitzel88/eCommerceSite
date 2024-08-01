891b27ccb47704f244c445a60b06971c418c0f7e
Brandon <brandonmitzel05@gmail.com>
2024-07-31 22:46:34 -0700

    Configured DB Context

--- eCommerceSite/Program.cs
+++ eCommerceSite/Program.cs
@@ -1,6 +1,14 @@
+using eCommerceSite.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<VintageContext>(options => 
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
